f14435ef328cd3b8f6c235af8f54083d74fd5d52
Pawel Urban <urbanpawel90@gmail.com>
2017-12-09 08:17:13 +0100

    Initial commit.

---
+++ server/src/main/java/com/urbanpawel/overtime/OvertimeApplication.java
@@ -0,0 +1,12 @@
+package com.urbanpawel.overtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvertimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OvertimeApplication.class, args);
+	}
+}
