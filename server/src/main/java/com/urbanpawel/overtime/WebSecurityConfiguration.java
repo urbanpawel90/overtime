f1a85f36fb82e9021eafdc6b3e5076132f83c549
Pawel Urban <urbanpawel90@gmail.com>
2017-12-21 08:53:44 +0100

    Disabled CSRF and Session management as it's not used in REST.

--- server/src/main/java/com/urbanpawel/overtime/WebSecurityConfiguration.java
+++ server/src/main/java/com/urbanpawel/overtime/WebSecurityConfiguration.java
@@ -4,12 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/").permitAll();
+        http.authorizeRequests().antMatchers("/**").permitAll();
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
