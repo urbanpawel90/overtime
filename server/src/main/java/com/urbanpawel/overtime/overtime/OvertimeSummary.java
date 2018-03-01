4178b96cbbda26edde3220a2ddf6593357a506e1
Pawel Urban <urbanpawel90@gmail.com>
2018-03-01 06:26:37 +0100

    Convert OvertimeSummary JPA entity class to Lombok.

--- server/src/main/java/com/urbanpawel/overtime/overtime/OvertimeSummary.java
+++ server/src/main/java/com/urbanpawel/overtime/overtime/OvertimeSummary.java
@@ -1,5 +1,8 @@
 package com.urbanpawel.overtime.overtime;
 
+import lombok.*;
+import lombok.experimental.Tolerate;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -7,100 +10,56 @@ import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+@Data
+@Setter(AccessLevel.PRIVATE)
+@EqualsAndHashCode(of = {"date", "hours"})
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "overtime")
 public final class OvertimeSummary {
     @Id
     @GeneratedValue
-    private Integer id;
+    Integer id;
     @Column(name = "`date`", nullable = false, unique = true)
-    private LocalDate date;
+    LocalDate date;
     @Column(nullable = false)
-    private BigDecimal hours;
+    BigDecimal hours;
     @ElementCollection
-    private List<Change> changes;
+    List<Change> changes;
 
-    @SuppressWarnings("unused")
-    private OvertimeSummary() {
-    }
-
+    @Tolerate
     private OvertimeSummary(LocalDate date) {
         this(BigDecimal.ZERO, date, Collections.emptyList());
     }
 
+    @Tolerate
     OvertimeSummary(BigDecimal hours, LocalDate date, List<Change> changes) {
-        this.hours = hours;
-        this.date = date;
-        this.changes = Collections.unmodifiableList(changes);
-    }
-
-    private OvertimeSummary(Integer id, BigDecimal hours, LocalDate date, List<Change> changes) {
-        this(hours, date, changes);
-        this.id = id;
+        this(null, date, hours, changes);
     }
 
     public static OvertimeSummary emptySummary(LocalDate date) {
         return new OvertimeSummary(date);
     }
 
-    public BigDecimal getHours() {
-        return hours;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public List<Change> getChanges() {
-        return changes;
-    }
-
     public OvertimeSummary apply(OvertimeSummary change) {
-        return new OvertimeSummary(id, hours.add(change.getHours()), date,
+        return new OvertimeSummary(id, date, hours.add(change.getHours()),
                 Stream.concat(change.changes.stream(), changes.stream())
                         .sorted(Comparator.comparing(Change::getWhen).reversed())
                         .collect(Collectors.toList()));
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof OvertimeSummary)) return false;
-        OvertimeSummary that = (OvertimeSummary) o;
-        return Objects.equals(hours, that.hours) &&
-                Objects.equals(date, that.date);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(hours, date);
-    }
-
+    @Data
+    @Setter(AccessLevel.PRIVATE)
+    @NoArgsConstructor
+    @AllArgsConstructor
     @Embeddable
-    public static class Change {
-        private BigDecimal amount;
+    static class Change {
+        BigDecimal amount;
         @Column(name = "`when`")
-        private LocalDateTime when;
-
-        @SuppressWarnings("unused")
-        Change() {
-        }
-
-        Change(BigDecimal amount, LocalDateTime when) {
-            this.amount = amount;
-            this.when = when;
-        }
-
-        public BigDecimal getAmount() {
-            return amount;
-        }
-
-        LocalDateTime getWhen() {
-            return when;
-        }
+        LocalDateTime when;
     }
 }
