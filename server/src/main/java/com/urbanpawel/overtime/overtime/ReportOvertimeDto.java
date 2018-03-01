cc3698a6557a6b81f8384123079c6101bfd0da78
Pawel Urban <urbanpawel90@gmail.com>
2018-03-01 07:12:47 +0100

    Optimized Lombok usage in ReportOvertimeDto to shorten the class code.

--- server/src/main/java/com/urbanpawel/overtime/overtime/ReportOvertimeDto.java
+++ server/src/main/java/com/urbanpawel/overtime/overtime/ReportOvertimeDto.java
@@ -1,23 +1,14 @@
 package com.urbanpawel.overtime.overtime;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Value;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-@Value
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-class ReportOvertimeDto {
+@Data
+@Setter(AccessLevel.PRIVATE)
+@NoArgsConstructor
+final class ReportOvertimeDto {
     LocalDate date;
     BigDecimal hours;
-
-    @JsonCreator
-    @SuppressWarnings("unused")
-    public static ReportOvertimeDto create(@JsonProperty("date") LocalDate date, @JsonProperty("hours") BigDecimal hours) {
-        return new ReportOvertimeDto(date, hours);
-    }
 }
